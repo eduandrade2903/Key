05ee0c09caf93c0dd2222dcedfe4bf24b8a0cdcf
Eduardo <eduandrade.lara@hotmail.com>
2025-03-19 14:13:09 -0300

    Adicionado na branch para features

--- src/main/java/Repository/EmployeeRepository.java
+++ src/main/java/Repository/EmployeeRepository.java
@@ -3,6 +3,8 @@ package Repository;
 import Model.TblEmployee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<TblEmployee, Integer> {
 
 }
