872775fb3795cc56f3a2d484be2c623fcb564048
Eduardo <eduandrade.lara@hotmail.com>
2025-04-09 18:41:49 -0300

    Mapper

--- src/main/java/Trab/Mapper/MapperConfig.java
+++ src/main/java/Trab/Mapper/MapperConfig.java
@@ -1,5 +1,6 @@
 package Trab.Mapper;
 
+
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,5 +11,4 @@ public class MapperConfig {
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
-
 }
